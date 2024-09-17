5a9cefc4e0d7fd95974356f5bcaa2e5d394f69f1
glennka1337 <viktor.h@outlook.com>
2024-09-17 15:00:58 +0200

    Trying out something.

--- Labb2_SUT24/Program.cs
+++ Labb2_SUT24/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Mata in en siffra: ");
+            int userInput = Int32.Parse(Console.ReadLine());
+            
+            ChessSize(userInput);
+        }
+
+        public static void ChessSize(int xy)
+        {
+            for (int i = 1; i <= xy; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("0");
+                }
+            }
         }
     }
 }
