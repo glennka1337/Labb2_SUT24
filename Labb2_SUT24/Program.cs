aed6e59034e32c500f0d0814e8e926c0b07d56f0
glennka1337 <viktor.h@outlook.com>
2024-09-17 14:08:04 +0200

    Add project files.

---
+++ Labb2_SUT24/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Labb2_SUT24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
